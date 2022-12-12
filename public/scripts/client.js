50ea4b636d2fff6776fa614f9a6ebeaec5b3d722
sophia kidawai <sophia.kidwai@telus.com>
2022-12-12 13:40:58 -0800

    Implemented createTweetElement and renderTweets function, styling needs to be redone

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,70 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+
+const data = [
+  {
+    user: {
+      name: "Newton",
+      avatars: "https://i.imgur.com/73hZDYK.png",
+      handle: "@SirIsaac",
+    },
+    content: {
+      text: "If I have seen further it is by standing on the shoulders of giants",
+    },
+    created_at: 1461116232227,
+  },
+  {
+    user: {
+      name: "Descartes",
+      avatars: "https://i.imgur.com/nlhLi3I.png",
+      handle: "@rd",
+    },
+    content: {
+      text: "Je pense , donc je suis",
+    },
+    created_at: 1461113959088,
+  },
+];
+
+const createTweetElement = function (tweet) {
+  const $tweet = $(`
+  <article class="tweet">
+  <header class="tweet-header">
+    <div class="profile-pic">
+      <img src="${tweet.user.avatars}">
+    </div>
+    <div class="name">
+      <h2>${tweet.user.name}</h2>
+      <p>${tweet.user.handle}</p>
+    </div>
+    <div class="handle">
+      <p>${tweet.created_at}</p>
+    </div>
+  </header>
+  <div class="tweet-content">
+    <p>${tweet.content.text}</p>
+  </div>
+  <footer class="tweet-footer">
+    <div class="icons">
+      <i class="fa-solid fa-flag"></i>
+      <i class="fa-sharp fa-solid fa-retweet"></i>
+      <i class="fa-solid fa-heart"></i>
+    </div>
+  
+  </footer>
+  </article>
+  `);
+  return $tweet;
+};
+
+const renderTweets = function (tweets) {
+  for (let tweet of tweets) {
+    const $tweet = createTweetElement(tweet);
+    $(".tweet-container").prepend($tweet);
+  }
+};
+
+$(document).ready(function () {
+  renderTweets(data);
+});
